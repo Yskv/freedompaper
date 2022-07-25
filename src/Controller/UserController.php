7b8f471e4940481ded10232c2dc7b9f36b68c3cb
Aimerick <aimerickwas@gmail.com>
2022-07-25 22:05:21 +0200

    WIP

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,22 +2,23 @@
 
 namespace App\Controller;
 
-use App\Entity\Item;
-use App\Form\ItemType;
+use App\Entity\User;
 use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/item')]
-class Userontroller extends AbstractController
+class UserController extends AbstractController
 {
     #[Route('/inscription', name: 'app_create_user', methods: ['GET'])]
     public function index(ItemRepository $itemRepository): Response
     {
-        return $this->render('item/index.html.twig', [
-            'items' => $itemRepository->findAll(),
+        $user = new User();
+        $form = $this->createFormBuilder($user);
+
+        return $this->render('user/index.html.twig', [
+            'form' => $form->getForm(),
         ]);
     }
 }
